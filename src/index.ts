0c810871a4c7b3178f8b77935dffbfa757cea36a
profchenmu <profchenmu@me.com>
2019-12-12 00:35:27 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import * as d3 from 'd3';
+import moment from 'moment';
 import socketIOClient from 'socket.io-client';
 import './style/index.scss';
+
+const dataTime = d3.select('#data-time');
+setInterval(()=>{
+    const timeInfo = moment().format('YYYY-MM-DD hh:mm:ss')
+    console.log(timeInfo);
+    dataTime.text(timeInfo)
+},1000)
 
 
